5676a8a3a7b8111ad8bbebc1d88e1d6f5670921f
Damien Nguyen <damien1@huawei.com>
2022-06-07 11:11:47 +0200

    [CMAKE] Better error message when failing to find m4

--- third_party/cmake/modules/Findm4.cmake
+++ third_party/cmake/modules/Findm4.cmake
@@ -18,6 +18,23 @@
 
 find_program(M4_EXEC m4 PATHS /usr/local/bin /usr/bin /bin /sbin)
 
+get_cmake_property(_variableNames VARIABLES)
+list(SORT _variableNames)
+foreach(_variableName ${_variableNames})
+  message(STATUS "${_variableName}=${${_variableName}}")
+endforeach()
+
+if(NOT M4_EXEC AND m4_FIND_REQUIRED)
+  message(
+    SEND_ERROR "Unable to locate m4 executable. "
+               "Try to install it and re-run CMake:\n"
+               "  - Ubuntu/Debian: sudo apt-get install m4\n"
+               "  - ArchLinux: sudo pacman -S m4\n"
+               "  - Fedora/CentOS: sudo yum install m4 or sudo dnf install m4\n"
+               "  - MacOS Homebrew: brew install m4\n"
+               "  - MacOS MacPorts: sudo port install m4")
+endif()
+
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(m4 REQUIRED_VARS M4_EXEC)
